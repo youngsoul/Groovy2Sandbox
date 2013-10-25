14c03f821d3795fca3f8843be7d1b1cd0ddaba23
Patrick Ryan <youngsoul@inbox.com>
2013-10-25 15:09:23 -0500

    Update MapExample.groovy

--- src/MapExample.groovy
+++ src/MapExample.groovy
@@ -1,10 +1,35 @@
-
 /**
  * Created by IntelliJ IDEA.
  * User: youngsoul
  * Date: 10/25/13
  * Time: 10:40 AM
  */
+
+/*
+
+Sorting maps is as easy as sorting lists.
+
+Map has 3 sort methods:
+
+sort() <- no params and performs a natural sort of the keys and returns a new map
+
+sort(Comparator) <- uses the comparator implementation to sort the keys
+
+sort(Closure) <- passes elements (key,value) into the closure for sorting.
+
+Below is an example of those calls.  Notice the one with Comparator since it is using the 'as' operator to dynamically create an instance of the Comparator interface from the closure.  It does not get any groovier than that to get an implementation of an interface.
+
+Also notice in the sort that uses a closure there are no parens for the function call because if a Closure is the last (or only in this case ) parameter to a function the function parens can be dropped.
+
+<=> is the Spaceship operator
+Spaceship <=> Useful in comparisons, returns -1 if left is smaller 0 if == to right or 1 if greater than the right
+
+*. is the Spread operator
+Used to invoke an action on all items of an aggregate object
+
+
+*/
+
 
 Map personMap = [name: 'bob', age: 30, employed: true, salary: 50000.01 ]
 
